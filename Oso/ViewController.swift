3c4690f124f4252447df05901ed693674fd06e39
Matthew Cline <mcline@berkeley.edu>
2015-11-08 23:54:19 -0800

    Commit test file

---
+++ Oso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oso
+//
+//  Created by Matthew Cline on 11/8/15.
+//  Copyright © 2015 Matthew Cline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
